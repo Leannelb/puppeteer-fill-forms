fb94b7c2c0c391cf65493964b6426791c27c67db
leanne <leanne.lacey@justsomecoding.com>
2018-11-13 17:48:18 +0100

    puppeteer working

--- test.js
+++ test.js
@@ -1,16 +1,22 @@
 const puppeteer = require('puppeteer');
-(async () => {
+
+// (async () => {
+async function run() {
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 10,
-    args: ['--start-fullscreen']
+    slowMo: 100,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    ignoreHTTPSErrors: true, 
+    dumpio: false,
+    devtools: true 
   });
-  const page = await browser.newPage();
+
+  var page = await browser.newPage(); 
   const USERNAME_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(3) > input'
   const PASS_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(4) > input'
   const LOGIN_BUTTON = 'body > app-root > app-login > div > div.content > form > div.create-account.yellow-gold'
   const HAMBURGER_ICON = 'body > app-root > div > app-navigation > div > div > a'
-  const SEARCH_FIELD = '#typeahead_example_1'
+  // const SEARCH_FIELD = '#typeahead_example_1' 
   const appUrlBase = 'https://slm.netlify.com/'
   const routes = {
     public: {
@@ -21,6 +27,8 @@ const puppeteer = require('puppeteer');
     },
   }
 
+  
+  
   //Go to my page and wait until the page loads
   await page.goto('https://slm.netlify.com/', {waitUntil: 'networkidle2'})
   //type the name
@@ -32,29 +40,16 @@ const puppeteer = require('puppeteer');
   //submit by clicking login
   await page.click(LOGIN_BUTTON)
   //LOGGED IN
+  await page.waitFor(2000)   
 
-  await page.waitForNavigation({waitUntil: 'networkidle'});
-    // await page.focus(SEARCH_FIELD)
-  // page.keyboard.type('villa@admin.com')
-  console.log('Hello')
+  console.log("AGGGGJKKKK")
 
-  // page.focus(SEARCH_FIELD),
-  // page.keyboard.type('villa@admin.com')
-  // await page.waitForNavigation({waitUntil: 'load'});
 
-  // await page.waitForNavigation();
-  // page.focus(SEARCH_FIELD),
-  // page.keyboard.type('villa@admin.com')
-  // await page.waitForNavigation();
-  // console.log('The wait is over 2');
+  await page.click('body > app-root > div > app-navigation > div > div > a')
+  await page.click('body > app-root > div > div.page-container > app-sidebar > div > div > ul > li.nav-item.ng-star-inserted.open.active > a')
 
-  // page.focus(SEARCH_FIELD),
-  // page.keyboard.type('villa@admin.com')
+  await page.keyboard.type('villa@admin.com')
 
-  // await Promise.all([
-  //   page.waitForNavigation({ waitUntil: 'networkidle2' }),
-  //   page.focus(SEARCH_FIELD),
-  //   page.keyboard.type('villa@admin.com')
-  // ]);
 
-})();
+}
+run();
