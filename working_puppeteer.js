988f779aee0323ec51ae723344927c0f1233147b
leanne <leanne.lacey@justsomecoding.com>
2018-11-29 17:46:01 +0100

    new changes still not working

--- working_puppeteer.js
+++ working_puppeteer.js
@@ -1,61 +1,49 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 10,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        ignoreHTTPSErrors: true, 
-        dumpio: false,
-        devtools: true 
-    });
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 10,
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            ignoreHTTPSErrors: true, 
+            dumpio: false,
+            devtools: true 
+        });
     
-    var page = await browser.newPage(); 
-    await page.setViewport({width: 2000, height: 2000})
-    //define all constants and selectors
-    // const USERNAME_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(3) > input'
-    // const PASS_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(4) > input'
-    // const LOGIN_BUTTON = 'body > app-root > app-login > div > div.content > form > div.create-account.yellow-gold'
-   
-    await page.goto('http://shortletsmalta.com/all_properties_menu.html', {waitUntil: 'networkidle2'})
-    await page.click('div.column:nth-child(5) > h3:nth-child(1) > a:nth-child(1)')
+        var page = await browser.newPage(); 
+        await page.setViewport({width: 2000, height: 2000})
+        //define all constants and selectors
+        const SHORTLETS_URL = 'http://shortletsmalta.com/all_properties_menu.html'
+        const BOOK_NOW = '#block-1 > div.column.e-1.odd.single.features_list.property > p > em > a'
+        const AMENITIES_PANNEL ='#block-4';
+        const AMENITIES_PANNEL_LI = '#block-4 > div.column.e-1.odd.single.description-prop_info-amenities > ul > li'
+        // const USERNAME_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(3) > input'
+        // const PASS_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(4) > input'
+        // const LOGIN_BUTTON = 'body > app-root > app-login > div > div.content > form > div.create-account.yellow-gold'
     
-    await page.focus('#property-1013476')
-    await page.focus('#content')
-    await page.focus('#block-4')
-    await page.focus('.description-prop_info-amenities > ul:nth-child(2)')
-    
-    const result = await page.evaluate(() => {
-        let amenity = ('.description-prop_info-amenities-5 > span:nth-child(1)').innerText;
-
-        console.log(amenity);
-    });
-
-    
-    // await page.keyboad.type('villa@admin.com')
-    // await page.focus(PASS_LOGIN_FIELD)
-    // await page.keyboard.type('123456')
-    // await page.click(LOGIN_BUTTON)
-    // await page.waitFor(2000) 
-
-    // for(var i = 0, b = 0; i < uuids.length, b < addresses.length; i++, b++){
-    //     await page.goto(uuids[i])
-    //     await page.waitFor(5000) 
+        await page.goto(SHORTLETS_URL, {waitUntil: 'networkidle2'})
+        await page.click(BOOK_NOW) 
+        await page.waitFor(2000) 
+        await page.focus(AMENITIES_PANNEL)
         
-    //     try 
-    //     {
-    //         await page.waitFor(2000) 
-    //         await page.click(PROPERIES_LOCATION_TAB)
-    //     } catch (error) {
-    //     console.log("darniooooo")
-    //     }
-    //     await page.click(ADD_NEW_PROPERIES_BUTTON)
-    //     await page.focus(DOOR_NO_FIELD)
-    //     await page.keyboard.type(addresses[b])
-    //     await page.waitFor(1000)
-    //     await page.click(SUBMIT_NEW_PROPERTY_BUTTON)
-    //     await page.waitFor(10000) 
-// }
+        await page.waitForSelector(AMENITIES_PANNEL);
+
+        const results = await page.evaluate(() => {
+            
+            page.focus('#block-4 > div.column.e-1.odd.single.description-prop_info-amenities > ul')
+            const elements = document.querySelectorAll('#block-4 > div.column.e-1.odd.single.description-prop_info-amenities > ul > li.description-prop_info-amenities-5.e-1.odd.first');
+            const amenitiesList = [];
+          
+            for (const amenitySingle of elements) {
+                amenitiesList.push(amenitySingle.innerText);
+            }
+            return amenitiesList;
+          });
+          console.dir(results)
+          console.dir(results)
+
+    
+
 // browser.close();
 
 }
