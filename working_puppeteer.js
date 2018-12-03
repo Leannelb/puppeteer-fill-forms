79180f7730e90f42b26e3cfea1e3d20249c79f31
leanne <leanne.lacey@justsomecoding.com>
2018-12-03 13:56:26 +0100

    urls pulled from db

--- working_puppeteer.js
+++ working_puppeteer.js
@@ -19,22 +19,57 @@ async function run() {
   const AMENITIES_PANNEL = "#block-4";
   const AMENITIES_PANNEL_LI =
     "#block-4 > div.column.e-1.odd.single.description-prop_info-amenities > ul > li";
-  // const USERNAME_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(3) > input'
-  // const PASS_LOGIN_FIELD = 'body > app-root > app-login > div > div.content > form > div:nth-child(4) > input'
-  // const LOGIN_BUTTON = 'body > app-root > app-login > div > div.content > form > div.create-account.yellow-gold'
 
   await page.goto(SHORTLETS_URL, { waitUntil: "networkidle2" });
   await page.click(BOOK_NOW);
   await page.waitFor(2000);
   await page.focus(AMENITIES_PANNEL);
 
-  const amenities = await page.evaluate(() =>
+  //  The evaluates below get the amentites list (from their classes) and creates an array with them.
+
+  const amenitiesEven = await page.evaluate(() =>
     Array.from(document.querySelectorAll("div.column li.even span")).map(
-      partner => partner.innerHTML
+      attribute => attribute.innerHTML
     )
   );
 
+  const amenitiesOdd = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("div.column li.odd span")).map(
+      attribute => attribute.innerHTML
+    )
+  );
+
+  const amenities = amenitiesEven.concat(amenitiesOdd);
   console.log(amenities);
+
+  const mysql = require("mysql");
+  const options = {
+    user: "root",
+    password: "root",
+    database: "slm_all_tables_2"
+  };
+  const connection = mysql.createConnection(options);
+
+  connection.connect(err => {
+    if (err) {
+      console.error("An error occurred while connecting to the DB");
+      throw err;
+    }
+  });
+
+  connection.query(
+    "SELECT temp_old_reference FROM property",
+    (error, property) => {
+      if (error) {
+        console.error("An error occurred while executing the query");
+        throw error;
+      }
+
+      console.log(property);
+    }
+  );
+
+  browser.close();
 }
 
 run();
