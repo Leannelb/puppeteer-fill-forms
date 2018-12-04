2bfe92aaf9f5a9ca251fc4093a9d905aea166110
leanne <leanne.lacey@justsomecoding.com>
2018-12-04 16:06:53 +0100

    code tidyied up and unnesesary files removed

--- working_puppeteer.js
+++ working_puppeteer.js
@@ -1,6 +1,54 @@
 const puppeteer = require("puppeteer");
 
-async function run() {
+const mysql = require("mysql");
+const options = {
+  user: "root",
+  password: "root",
+  database: "slm_all_tables_2"
+};
+const connection = mysql.createConnection(options);
+var slm_urls = [];
+connection.connect(err => {
+  if (err) {
+    console.error("An error occurred while connecting to the DB");
+    throw err;
+  }
+});
+
+class DataFatcher {
+  async getData() {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "SELECT temp_old_reference, id FROM property",
+        (error, dbResult) => {
+          if (error) {
+            console.error("An error occurred while executing the query");
+            resolve(null);
+          } else {
+            resolve(dbResult);
+          }
+        }
+      );
+    });
+  }
+}
+
+//aarons code
+var dbFetcher = new DataFatcher();
+dbFetcher.getData().then(result => {
+  slm_urls = result;
+  looping();
+});
+
+function looping() {
+  openWeb(slm_urls[0]);
+}
+
+function openWeb(property) {
+  run(property);
+}
+
+async function run(property) {
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 10,
@@ -10,29 +58,23 @@ async function run() {
     devtools: true
   });
 
+  const util = require("util");
+  // console.log(util.inspect(property, { showHidden: false, depth: null }));
+  var current_prop_url = property.temp_old_reference;
   var page = await browser.newPage();
   await page.setViewport({ width: 2000, height: 2000 });
-  //define all constants and selectors
-  const SHORTLETS_URL = "http://shortletsmalta.com/all_properties_menu.html";
-  const BOOK_NOW =
-    "#block-1 > div.column.e-1.odd.single.features_list.property > p > em > a";
   const AMENITIES_PANNEL = "#block-4";
-  const AMENITIES_PANNEL_LI =
-    "#block-4 > div.column.e-1.odd.single.description-prop_info-amenities > ul > li";
 
-  await page.goto(SHORTLETS_URL, { waitUntil: "networkidle2" });
-  await page.click(BOOK_NOW);
+  await page.goto(current_prop_url, { waitUntil: "networkidle2" });
   await page.waitFor(2000);
   await page.focus(AMENITIES_PANNEL);
 
   //  The evaluates below get the amentites list (from their classes) and creates an array with them.
-
   const amenitiesEven = await page.evaluate(() =>
     Array.from(document.querySelectorAll("div.column li.even span")).map(
       attribute => attribute.innerHTML
     )
   );
-
   const amenitiesOdd = await page.evaluate(() =>
     Array.from(document.querySelectorAll("div.column li.odd span")).map(
       attribute => attribute.innerHTML
@@ -40,36 +82,15 @@ async function run() {
   );
 
   const amenities = amenitiesEven.concat(amenitiesOdd);
-  console.log(amenities);
-
-  const mysql = require("mysql");
-  const options = {
-    user: "root",
-    password: "root",
-    database: "slm_all_tables_2"
-  };
-  const connection = mysql.createConnection(options);
-
-  connection.connect(err => {
-    if (err) {
-      console.error("An error occurred while connecting to the DB");
-      throw err;
-    }
-  });
-
-  connection.query(
-    "SELECT temp_old_reference FROM property",
-    (error, property) => {
-      if (error) {
-        console.error("An error occurred while executing the query");
-        throw error;
-      }
-
-      console.log(property);
-    }
-  );
-
   browser.close();
+
+  // insertIntoDB(amenities, property);
 }
 
-run();
+// function insertIntoDB(property, amenities) {
+//   var url = property.temp_old_reference;
+//   var id = property.id;
+//   console.log(
+//     "property URL" + url + "property ID " + id + "amenities" + amenities
+//   );
+// }
