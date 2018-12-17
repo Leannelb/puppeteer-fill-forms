dc368068fd88ddc82a55fed6fa01512d4e5352c2
leanne <leanne.lacey@justsomecoding.com>
2018-12-17 18:27:12 +0100

    script working, pulling all city information

--- cities_pull.js
+++ cities_pull.js
@@ -40,8 +40,12 @@ dbFetcher.getData().then(result => {
 });
 
 function looping() {
-  openWeb(slm_urls[0]);
-  slm_urls.shift();
+  if (slm_urls.length > 0) {
+    openWeb(slm_urls[0]);
+    slm_urls.shift();
+  } else {
+    console.log("WOOOOW :)");
+  }
 }
 
 function openWeb(property) {
@@ -59,29 +63,65 @@ async function run(property) {
   });
 
   var current_prop_url = property.temp_old_reference;
-  var current_prop_id = property.id;
-  var page = await browser.newPage();
-  await page.setViewport({ width: 2000, height: 2000 });
-  const AMENITIES_PANNEL = "#block-4";
+  if (
+    current_prop_url !== null &&
+    current_prop_url !==
+      "http://shortletsmalta.com/034-Stunning-3-bedroom-Duplex-Penthouse.html" &&
+    current_prop_url !=
+      "http://shortletsmalta.com/090_St'Julians_Hill_Duplex_Pth" &&
+    current_prop_url !=
+      "http://shortletsmalta.com/087_St'Julians_Hill_1-bedroom_Apt" &&
+    current_prop_url.indexOf("http") > -1
+  ) {
+    var current_prop_id = property.id;
+    var page = await browser.newPage().catch(() => (page = null));
+    await page.setViewport({ width: 2000, height: 2000 });
+    const AMENITIES_PANNEL = "#block-4";
 
-  await page.goto(current_prop_url, { waitUntil: "networkidle2" });
-  await page.waitForSelector("#content > #block-3 > .column > .paragraph");
+    await page
+      .goto(current_prop_url, { waitUntil: "networkidle2" })
+      .catch(() => (page = null));
 
-  //  The evaluates below get the amentites list (from their classes) and creates an array with them.
-  const cities = await page.evaluate(() =>
-    Array.from(
-      document.querySelectorAll("#content > #block-3 > .column > .paragraph")
-    ).map(paragraph => paragraph.innerHTML)
-  );
+    if (page != null) {
+      await page
+        .waitForSelector("#content > #block-3 > .column > .paragraph")
+        .catch(() => (page = null));
+      if (page != null) {
+        //  The evaluates below get the amentites list (from their classes) and creates an array with them.
+        cites = [];
+        try {
+          cities = await page
+            .evaluate(() =>
+              Array.from(
+                document.querySelectorAll(
+                  "#content > #block-3 > .column > .paragraph"
+                )
+              ).map(paragraph => paragraph.innerHTML)
+            )
+            .catch(e => "");
+        } catch (e) {
+          console.log("An errror has occured and handled kinda", e);
+        }
 
-  console.log(
-    "The paragraph of info for " +
-      current_prop_url +
-      "\t\n" +
-      "  is " +
-      cities +
-      "\t\n\n\t\n\t\n\r\r\r\r\r"
-  );
-  browser.close();
-  looping();
+        console.log(
+          "The paragraph of info for " +
+            current_prop_url +
+            "\t\n" +
+            "  is " +
+            cities +
+            "\t\n\n\t\n\t\n\r\r\r\r\r"
+        );
+        browser.close();
+        looping();
+      } else {
+        console.log("SHIT, Wait for selector ");
+      }
+    } else {
+      console.log("SHIT, GOTO PAGE ");
+    }
+  } else {
+    console.log("SHIT", current_prop_url);
+    browser.close();
+    looping();
+  }
 }
